1747d26faef48b39088085d16b78210c87f53898
guanyingcao <583816604@qq.com>
2018-12-12 14:59:39 +0800

    first commit

---
+++ easyviews/src/main/java/com/easyviews/views/alertview/OnDismissListener.java
@@ -0,0 +1,12 @@
+package com.easyviews.views.alertview;
+
+/**
+ * package: com.easyviews.views.alertview.OnDismissListener
+ * author: gyc
+ * description:
+ * time: create at 2017/3/6 22:33
+ */
+
+public interface OnDismissListener {
+    void onDismiss(Object o);
+}
---
+++ easyviews/src/main/java/com/easyviews/views/alertview/OnItemClickListener.java
@@ -0,0 +1,12 @@
+package com.easyviews.views.alertview;
+
+/**
+ * package: com.easyviews.views.alertview.OnItemClickListener
+ * author: gyc
+ * description:
+ * time: create at 2017/3/6 22:32
+ */
+
+public interface OnItemClickListener {
+    void onItemClick(Object o, int position);
+}
